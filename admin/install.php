52c5b6f14f5227c440913b24f3b92a0a8cb41418
Hieg Khatcherian <hiegdk@gmail.com>
2012-10-22 15:13:43 -0700

    Update path to css in admin header

--- admin/install.php
+++ admin/install.php
@@ -1,54 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#">Download Codes</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li><a href="#">Home</a></li>
-              <li class="active"><a href="#">Admin</a></li>
-              <li><a href="#about">About</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-
-      <h1>Install Download Codes</h1>
-
 <?php
 require_once('../config.php');
+require_once('top.php');
+
+echo '<h1>Install Download Codes</h1>';
 
 //sql to create the db tables
 $sql_drop_albums = "DROP TABLE albums;";
@@ -87,7 +41,7 @@ if($_POST && $_POST['step'] == "createdb"){
 		printf("<div class=\"alert alert-important\">Error: %s</div>", $mysqli->error);
 	}
 
-	echo '<a href="../index.php" class="btn btn-large btn-success">Get Started...</a>';
+	echo '<a href="index.php" class="btn btn-large btn-success">Get Started...</a>';
 
 //check to see if we have good creds and the DB doesn't already exist
 }else{
@@ -126,10 +80,5 @@ if($_POST && $_POST['step'] == "createdb"){
 
 
 }
+require_once('bottom.php');
 ?>
-
-
-    </div> <!-- /container -->
-
-  </body>
-</html>
